e9bb097f78360e547c7b2beaa74cacf04723ad95
ahgddev <ahgddev@gmail.com>
2024-01-11 15:33:52 -0700

    Changed isOver25 to isUnder25, modified the variable to check if any number is over 25 by summing up all the numbers and checking if they're over 100, which shouldn't be possible of everything is under 25.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 + n2 + n3 + n4 < 100;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -30,7 +30,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
 console.log(isValid);
@@ -56,3 +56,4 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 // Find the remainder of dividing the result by the fourth number.
 
 const math_chain = ((n1-n2) * n3)/n4
+
